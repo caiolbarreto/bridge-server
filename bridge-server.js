302d75bfc5991e7018120f2ba192a7848cac54d7
Caio Bezerra Londres Barreto <caiobarreto_28@hotmail.com>
2024-04-19 16:05:20 -0300

    chore: add json server and handeling errors

--- bridge-server.js
+++ bridge-server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Define routes
 app.post('/send-to-esps', async (req, res) => {
@@ -12,7 +12,7 @@ app.post('/send-to-esps', async (req, res) => {
     res.status(200).send('Request forwarded to ESPs successfully.');
   } catch (error) {
     console.error('Error forwarding request to ESPs:', error);
-    res.status(500).send('Internal server error.');
+    res.status(500).send(`Internal server error. ${req.body}`);
   }
 });
 
@@ -26,16 +26,38 @@ app.get('/testing', async (_req, res) => {
     }
   });
 
+// Testing route
+app.get('/posts', async (_req, res) => {
+  try {
+    const axiosResponse = await axios.get('http://localhost:3000/posts');
+    // Send the data property of the axios response as the response to the client
+    res.status(200).json(axiosResponse.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error.');
+  }
+});
+
 // Function to send request to ESPs
 async function sendToESPs(data) {
-  // Assuming you have 6 ESPs running locally with different URLs
   const espURLs = [
     'http://192.168.1.100:8001/auth'
+    // Add URLs for other ESPs here
   ];
 
-  // Send request to each ESP asynchronously
-  const requests = espURLs.map(url => axios.post(url, data));
-  await Promise.all(requests);
+  try {
+    // Send request to each ESP asynchronously
+    const requests = espURLs.map(url => axios.post(url, data)
+      .catch(error => {
+        console.error(`Error sending request to ${url}:`, error);
+        throw error; // Re-throw the error for handling in the catch block
+      }));
+    await Promise.all(requests);
+    console.log('Requests sent to ESPs successfully.');
+  } catch (error) {
+    console.error('Error sending requests to ESPs:', error);
+    throw error; // Re-throw the error for handling in the caller function
+  }
 }
 
 // Start the server
